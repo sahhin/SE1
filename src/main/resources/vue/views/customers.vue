767bb189059539c22490b96297b6a174c1db4ad0
Jan Dalski <jan.dalski@haw-hamburg.de>
2020-06-01 13:28:49 +0200

    POST-Beispiel hinzugefügt (Kundenerstellung). HTTP-Funktionalität auf die Axios-Bibliothek umgestellt.

--- src/main/resources/vue/views/customers.vue
+++ src/main/resources/vue/views/customers.vue
@@ -25,6 +25,31 @@
         </tr>
       </tbody>
     </table>
+
+    <h2>Neuen Kunden anlegen</h2>
+    <form id="formCreateCustomer" @submit="processForm">
+      <div id="formErrorMsg" v-if="errors.length">
+        <h3>Bitte korrigiere die folgenden Fehler:</h3>
+        <ul>
+          <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+        </ul>
+      </div>
+      <p>
+        <label for="cFirstName">Vorname:</label>
+        <input id="cFirstName" v-model="newCustomer.firstName" type="text" />
+      </p>
+      <p>
+        <label for="cLastName">Nachname:</label>
+        <input id="cLastName" v-model="newCustomer.lastName" type="text" />
+      </p>
+      <p>
+        <label for="cEmail">E-Mail:</label>
+        <input id="cEmail" v-model="newCustomer.email" type="text" size="30" />
+      </p>
+      <p>
+        <input type="submit" value="Abschicken" />
+      </p>
+    </form>
   </app-frame>
 </template>
 
@@ -32,19 +57,66 @@
 <script>
   Vue.component("customers-comp", {
     template: "#customers-tmpl",
+
     data: () => ({
-      customers: []
+      newCustomer: {},
+      customers: [],
+      errors: []
     }),
+
     created() {
-      fetch("/api/customers")
-        .then(res => res.json())
-        .then(res => {
-          this.customers = res;
-          //console.log(res);
-        })
-        .catch(() => alert("Error while fetching customers"));
-      console.log("CustomerFrame created. Calling emit!");
-      //this.$emit('enlarge-text', 0.1);
+      this.loadCustomers();
+    },
+
+    methods: {
+
+      /** Load all customers from the REST endpoint. */
+      loadCustomers: function() {
+        axios.get("/api/customers")
+          .then(response => {
+            this.customers = response.data;
+          }).catch(() => {
+            alert("Error while fetching customers");
+          });
+      },
+
+      /** Process the input form to create a new customer. */
+      processForm: function(e) {
+
+        // Validate the user's input.
+        this.errors = [];
+        if (!this.newCustomer.firstName) this.errors.push('Vorname fehlt');
+        if (!this.newCustomer.lastName)  this.errors.push('Nachname fehlt');
+        if (!this.newCustomer.email)     this.errors.push('E-Mail Adresse fehlt');
+        else if (!this.validEmail(this.newCustomer.email)) {
+          this.errors.push('E-Mail Adresse ist ungültig');
+        }
+
+        // Input validation successful! Send POST request to the backend.
+        if (this.errors.length == 0) {
+          axios.post("/api/customers", {
+            firstName: this.newCustomer.firstName,
+            lastName: this.newCustomer.lastName,
+            emailAddress: { email: this.newCustomer.email }
+          }).then(response => {
+            console.log("POST successful.");  // Got a success code as response (201).
+            this.loadCustomers();             // Reload the customer table.
+            this.newCustomer = {};            // Clear input fields.
+          }, error => {
+            console.error("POST failed! Error:");  // Something failed.
+            console.error(error);                  // Print error message on console.
+          });
+        }
+        e.preventDefault();
+      },
+
+      /** Validate an e-mail address.
+       * @param email Input to be checked if it is a valid e-mail expression.
+       * @returns 'true' if the e-mail check was successful, 'false' otherwise. */
+      validEmail: function(email) {
+        var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+      }
     }
   });
 </script>
@@ -53,5 +125,28 @@
 <style>
   table th {
     background-color: lightgrey;
+  }
+  #formCreateCustomer label {
+    float: left;
+    min-width: 100px;
+  }
+  #formCreateCustomer input {
+    font-family: monospace;
+    font-size: 0.9em;
+  }
+  #formErrorMsg {
+    color: red;
+    background-color: rgb(255, 210, 210);
+    border: 1px solid rgb(160, 0, 0);
+    border-radius: 8px;
+    margin-bottom: 20px;
+    padding: 2px 15px;
+    width: 350px;
+  }
+  #formErrorMsg h3 {
+    margin: 5px 2px;
+  }
+  #formErrorMsg li {
+    color: red;
   }
 </style>
